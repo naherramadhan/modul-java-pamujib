f74e63769ebf04d73e51a57418c4e43dba02263c
naherramadhan <naherramadhan07@gmail.com>
2024-08-13 10:37:11 +0700

    Add files via upload

---
+++ Pertemuan_2.1/src/pertemuan_2/pkg1/Pertemuan_21.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan_2.pkg1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan_21 {
+   public static String Hobi;
+    public static final String Hewan = "Ikan Hias";
+    
+    // Menambahkan variabel statis baru
+    public static final String Makanan = "Ayam Geprek";
+    public static final String Minuman = "Jus Jambu";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Hobi = "Sepak Bola";// Mengisi Value pada variable Hobi
+        System.out.println("Hobi Saya " + Hobi);// Memanggil Variable Hobi
+        System.out.println("Peliharaan Saya " + Hewan);// Memanggil Variable Hewan
+        
+        // Menambahkan output untuk variabel Makanan dan Minuman
+        System.out.println("Makanan Favorit saya : " + Makanan);
+        System.out.println("Minuman Favorit saya : " + Minuman);
+    }
+    
+}
---
+++ pertemuan2.2/src/pertemuan2/pkg2/Pertemuan22.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan2.pkg2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         makanan = "Ayam Geprek";
+        String minuman = "Jus Jambu";
+            System.out.println("Makanan Favorite saya : " + makanan) ;
+            System.out.println("Minuman Favorite Saya : " + minuman) ;
+    }
+    
+}
